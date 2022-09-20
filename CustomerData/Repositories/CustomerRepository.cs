c5c50fafb69b30ca317b7b561e133bfbae36794d
Milana Maksina <m_maksinaa@mail.ru>
2022-09-20 21:57:33 +0300

    feat: add interfaces for repos

--- CustomerData/Repositories/CustomerRepository.cs
+++ CustomerData/Repositories/CustomerRepository.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CustomerData.Contexts;
+using CustomerData.Entities;
 
 namespace CustomerData.Repositories
 {
-    internal class CustomerRepository
+    public class CustomerRepository: ICustomerRepository
     {
+        private readonly CustomerProjectContext _context;
+
+        public CustomerRepository(CustomerProjectContext context)
+        {
+            _context = context;
+        }
+
+        public Customer? GetCustomerById(int Id) => _context.Customers?.FirstOrDefault(c => c.Id == Id);
+        public List<Customer> GetCustomers() => _context.Customers.ToList();
+
+        public int CreateCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer.Id;
+        }
+
+        public void DeleteCustomer(int Id)
+        {
+            var customer = GetCustomerById(Id);
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+        }
+        public void UpdateCustomer(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            _context.SaveChanges();
+        }
+
     }
 }
---
+++ CustomerData/Repositories/Interfaces/IAddressRepository.cs
@@ -0,0 +1,14 @@
+﻿using CustomerData.Entities;
+
+namespace CustomerData.Repositories.Interfaces
+{
+    public interface IAddressRepository
+    {
+        int CreateAddress(Address entity);
+        List<Address> GetAddresses();
+        void DeleteAddress(int Id);
+        void UpdateAddress(Address entity);
+        Address? GetAddressById(int Id);
+
+    }
+}
