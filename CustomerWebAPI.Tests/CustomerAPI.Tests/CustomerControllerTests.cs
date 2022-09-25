be5dbef75e26efbe56e3a52adc6261824e649577
Milana Maksina <m_maksinaa@mail.ru>
2022-09-26 00:56:41 +0300

    feat: .gitignore

---
+++ CustomerWebAPI.Tests/CustomerAPI.Tests/CustomerControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerWebAPI.Tests.CustomerAPI.Tests
+{
+    internal class CustomerControllerTests
+    {
+    }
+}
